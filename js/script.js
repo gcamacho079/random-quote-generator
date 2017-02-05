64f514b2f23332268aeea281ac485bf1a498c111
Guadalupe Camacho <gcamacho079@gmail.com>
2017-02-05 11:06:23 -0800

    Break up pieces of functionality

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 var author;
 var quote;
 
-$(document).ready(function() {
-  console.log("HI!");
-  $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(json) {
-    author = JSON.stringify(json.author);
-    quote = JSON.stringify(json.quote);
-    author = author.substring(1, author.length - 1);
-    console.log(author);
-    $("#quoteAuthor").html(author);
-    console.log(quote);
-    $("#quoteText").html(quote);
-  });
+var randomQuote = {
+  init: function() {
+    randomQuote.getQuote();
+    console.log("HELLOW!")
+  },
 
-});
+  getQuote: function() {
+    $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(json) {
+      author = JSON.stringify(json.author);
+      quote = JSON.stringify(json.quote);
+      randomQuote.authorUnquote();
+      console.log(author);
+      $("#quoteAuthor").html(author);
+      console.log(quote);
+      $("#quoteText").html(quote);
+    });
+  },
+
+  authorUnquote: function() {
+    author = author.substring(1, author.length - 1);
+    return author;
+  }
+};
+
+$(document).ready(randomQuote.init);
