da8f5eba22eb68fa44878b07b45c0ddb7cd8ec67
Guadalupe Camacho <gcamacho079@gmail.com>
2017-02-04 22:34:53 -0800

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var author;
+var quote;
+
+$(document).ready(function() {
+  console.log("HI!");
+  $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(json) {
+    author = JSON.stringify(json.author);
+    quote = JSON.stringify(json.quote);
+    console.log(author);
+    $("#quoteAuthor").html(author);
+    console.log(quote);
+    $("#quoteText").html(quote);
+  });
+
+});
