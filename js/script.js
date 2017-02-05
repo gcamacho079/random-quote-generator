922dd738c81f4697953fda5ca234dafee8c608a9
Guadalupe Camacho <gcamacho079@gmail.com>
2017-02-05 10:42:05 -0800

    Make initial request for quote/author and print to console

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(json) {
     author = JSON.stringify(json.author);
     quote = JSON.stringify(json.quote);
+    author = author.substring(1, author.length - 1);
     console.log(author);
     $("#quoteAuthor").html(author);
     console.log(quote);
