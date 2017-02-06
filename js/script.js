1c46978420a97f0f836799df3623e33a23092e22
Guadalupe Camacho <gcamacho079@gmail.com>
2017-02-06 10:15:59 -0800

    Setup tweet function and initial length-dependent control flow

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var author;
 var quote;
+var totalLength;
 
 var randomQuote = {
   init: function() {
@@ -10,17 +11,38 @@ var randomQuote = {
     $.getJSON("https://cors-anywhere.herokuapp.com/http://quotes.stormconsultancy.co.uk/random.json", function(json) {
       author = JSON.stringify(json.author);
       quote = JSON.stringify(json.quote);
-      randomQuote.authorUnquote();
+      randomQuote.removeQuotes();
       $("#quoteAuthor").html(author);
       $("#quoteText").html(quote);
+      tweet.init();
     });
   },
 
-  authorUnquote: function() {
+  removeQuotes: function() {
     author = author.substring(1, author.length - 1);
     return author;
   }
-
 };
+
+var tweet = {
+  init: function() {
+    tweet.checkLength();
+  },
+
+  checkLength: function() {
+    totalLength = author.length + quote.length;
+    console.log(totalLength);
+    if (totalLength < 141) {
+      console.log("Let's tweet this!");
+    }
+    else {
+      tweet.shortenQuote();
+    }
+  },
+
+  shortenQuote: function() {
+    console.log("We've gotta work on this one...");
+  }
+}
 
 $(document).ready(randomQuote.init);
