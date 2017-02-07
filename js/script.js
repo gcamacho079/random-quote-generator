61daad376df9df22ca97d4b2f4cde3646e2e4ae5
Guadalupe Camacho <gcamacho079@gmail.com>
2017-02-07 11:34:10 -0800

    Reorganize functions between object literals, remove quote id var

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 var author;
 var quote;
-var quoteIdentifier;
 var totalLength;
 var tweetableQuote;
 
@@ -13,36 +12,38 @@ var randomQuote = {
     $.getJSON("https://cors-anywhere.herokuapp.com/http://quotes.stormconsultancy.co.uk/random.json", function(json) {
       author = JSON.stringify(json.author);
       quote = JSON.stringify(json.quote);
-      quoteID = JSON.stringify(json.id);
-      quoteID = parseInt(quoteID);
       randomQuote.removeQuotes();
       $("#quoteAuthor").html(author);
       $("#quoteText").html(quote);
-      tweet.init();
+      randomQuote.tweetabilityCheck();
     });
   },
 
   removeQuotes: function() {
     author = author.substring(1, author.length - 1);
     return author;
+  },
+
+  tweetabilityCheck: function() {
+    totalLength = author.length + quote.length;
+    console.log(totalLength);
+    if (totalLength < 137) {
+      tweetableQuote = quote + " - " + author;
+      $("#twitter-button").show();
+      console.log(tweetableQuote);
+    }
+    else {
+      $("#twitter-button").hide();
+    }
   }
 };
 
 var tweet = {
   init: function() {
-    tweet.checkLength();
+    tweet.makeLink();
   },
 
-  checkLength: function() {
-    totalLength = author.length + quote.length;
-    console.log(totalLength);
-    if (totalLength < 137) {
-      $("#twitter-button").show();
-      console.log(quote + " - " + author);
-    }
-    else {
-      $("#twitter-button").hide();
-    }
+  makeLink: function() {
   }
 };
 
